40dda4be953f9e09b843f2911c09f97b2b1228b5
Vsevolond <vsevolond@gmail.com>
2024-03-14 21:40:54 +0300

    LAB3: fix

--- NumericalMethods_Labs/lab3/main.swift
+++ NumericalMethods_Labs/lab3/main.swift
@@ -1,5 +1,4 @@
 import Foundation
-import Accelerate
 
 extension Float {
     
@@ -30,29 +29,48 @@ extension Array where Element == Float {
     func pairs() -> [(Float, Float)] {
         (1..<count).map { (self[$0 - 1], self[$0]) }
     }
+    
+    var minAll: (element: Float, indexes: [Int]) {
+        var min: (element: Float, indexes: [Int]) = (self[0], [0])
+        for i in 1..<count {
+            if self[i] < min.element {
+                min = (self[i], [i])
+            } else if self[i] == min.element {
+                min.indexes.append(i)
+            }
+        }
+        
+        return min
+    }
 }
 
 func slauSolution(matrixA: [[Float]], vectorB: [Float], size: Int) -> [Float] {
     var matrixT: [[Float]] = .init(repeating: .init(repeating: .zero, count: size), count: size)
-    matrixT[0][0] = sqrt(matrixA[0][0])
-    (1..<size).forEach { matrixT[0][$0] = matrixA[0][$0] / matrixT[0][0] }
-    (1..<size).forEach { i in
-        matrixT[i][i] = sqrt(matrixA[i][i] - (0..<i).map { matrixT[$0][i].pow(2) }.sum())
-    }
-    for j in 2..<size {
-        for i in 1..<j {
-            matrixT[i][j] = (matrixA[i][j] - (0..<i).map { matrixT[$0][i] * matrixT[$0][j] }.sum()) / matrixT[i][i]
+    
+    for i in 0..<size {
+        for j in 0...i {
+            
+            if i == j {
+                let sum = (0..<j).map { matrixT[j][$0].pow(2) }.sum()
+                matrixT[j][j] = sqrtf(matrixA[j][j] - sum)
+                
+            } else {
+                let sum = (0..<j).map { matrixT[i][$0] * matrixT[j][$0] }.sum()
+                matrixT[i][j] = (matrixA[i][j] - sum) / matrixT[j][j]
+            }
         }
     }
     
     var y: [Float] = .init(repeating: .zero, count: size)
     for i in 0..<size {
-        y[i] = (vectorB[i] - (0..<i).map { matrixT[$0][i] * y[$0] }.sum()) / matrixT[i][i]
+        let sum = (0..<i).map { matrixT[i][$0] * y[$0] }.sum()
+        y[i] = (vectorB[i] - sum) / matrixT[i][i]
     }
     
     var x: [Float] = .init(repeating: .zero, count: size)
-    for i in stride(from: size - 1, to: -1, by: -1) {
-        x[i] = (y[i] - (i+1..<size).map { matrixT[i][$0] * x[$0] }.sum()) / matrixT[i][i]
+    for i in stride(from: size - 1, through: 0, by: -1) {
+        let sum = (i+1..<size).map { matrixT[$0][i] * x[$0] }.sum()
+        x[i] = (y[i] - sum) / matrixT[i][i]
     }
     
     return x
@@ -95,16 +113,68 @@ for i in 0..<m {
 
 let lamda = slauSolution(matrixA: matrixA, vectorB: vectorB, size: m)
 
-let function = Polynom(factors: lamda)
-let variance = sqrt(xArray.enumerated().map { index, x in
-    (function.value(by: x) - yArray[index]).pow(2)
+let polynom = Polynom(factors: lamda)
+let variance = sqrtf(xArray.enumerated().map { index, x in
+    (polynom.value(by: x) - yArray[index]).pow(2)
 }.sum())
 
-let inaccuracy = variance / sqrt(Float(n))
-let error = inaccuracy / sqrt(yArray.map { $0.pow(2) }.sum())
-let middleValues = xArray.pairs().map { function.value(by: ($0 + $1) / 2) }
+let inaccuracy = variance / sqrtf(Float(n))
+let error = inaccuracy / sqrtf(yArray.map { $0.pow(2) }.sum())
+var middleValues = xArray.pairs().map { polynom.value(by: ($0 + $1) / 2) }
 
 print(matrixA, vectorB)
 print(lamda)
 print(inaccuracy, error)
+print(middleValues)
+
+let middleX: (arithmetic: Float, geometric: Float, harmonic: Float) = (
+    (xArray[0] + xArray[n - 1]) / 2,
+    sqrtf(xArray[0] * xArray[n - 1]),
+    2 / (1 / xArray[0] + 1 / xArray[n - 1])
+)
+let middleY: (arithmetic: Float, geometric: Float, harmonic: Float) = (
+    (yArray[0] + yArray[n - 1]) / 2,
+    sqrtf(yArray[0] * yArray[n - 1]),
+    2 / (1 / yArray[0] + 1 / yArray[n - 1])
+)
+
+let middleZ: (arithmetic: Float, geometric: Float, harmonic: Float) = (
+    polynom.value(by: middleX.arithmetic),
+    polynom.value(by: middleX.geometric),
+    polynom.value(by: middleX.harmonic)
+)
+
+let delta1 = abs(middleZ.arithmetic - middleY.arithmetic)
+let delta2 = abs(middleZ.geometric - middleY.geometric)
+let delta3 = abs(middleZ.arithmetic - middleY.geometric)
+let delta4 = abs(middleZ.geometric - middleY.arithmetic)
+let delta5 = abs(middleZ.harmonic - middleY.arithmetic)
+let delta6 = abs(middleZ.arithmetic - middleY.harmonic)
+let delta7 = abs(middleZ.harmonic - middleY.harmonic)
+let delta8 = abs(middleZ.harmonic - middleY.geometric)
+let delta9 = abs(middleZ.geometric - middleY.harmonic)
+
+let minAll = [delta1, delta2, delta3, delta4, delta5, delta6, delta7, delta8, delta9].minAll
+guard minAll.indexes.count == 1, let funcType = minAll.indexes.first else {
+    fatalError("error")
+}
+
+print(funcType + 1)
+
+func z(a: Float, b: Float, x: Float) -> Float {
+    return a * logf(x) + b
+}
+
+let a: Float = xArray.map { logf($0).pow(2) }.sum()
+let b: Float = xArray.map { logf($0) }.sum()
+let c: Float = xArray.enumerated().map { index, elem in
+    return logf(elem) * (1 / yArray[index])
+}.sum()
+let d: Float = yArray.map { 1 / $0 }.sum()
+
+let divisor: Float = b * b - a * Float(n)
+let alpha: Float = expf((d * b - c * Float(n)) / divisor)
+let betta: Float = 1 / ((c * b - a * d) / divisor)
+
+middleValues = xArray.pairs().map { z(a: alpha, b: betta, x: ($0 + $1) / 2) }
 print(middleValues)
