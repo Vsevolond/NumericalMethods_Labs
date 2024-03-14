af5e76e1d354b6c9eaa6629a6def3e91aecdcdbd
Vsevolond <vsevolond@gmail.com>
2024-03-14 09:51:54 +0300

    LAB3: some fix

--- NumericalMethods_Labs/lab3/main.swift
+++ NumericalMethods_Labs/lab3/main.swift
@@ -1,10 +1,10 @@
 import Foundation
 import Accelerate
 
-extension Double {
+extension Float {
     
-    func pow(_ value: Int) -> Double {
-        var result: Double = 1.0
+    func pow(_ value: Int) -> Float {
+        var result: Float = 1.0
         
         if value < 0 {
             for _ in 0..<abs(value) {
@@ -21,19 +21,19 @@ extension Double {
     }
 }
 
-extension Array where Element == Double {
+extension Array where Element == Float {
     
-    func sum() -> Double {
+    func sum() -> Float {
         reduce(into: 0) { $0 += $1 }
     }
     
-    func pairs() -> [(Double, Double)] {
+    func pairs() -> [(Float, Float)] {
         (1..<count).map { (self[$0 - 1], self[$0]) }
     }
 }
 
-func slauSolution(matrixA: [[Double]], vectorB: [Double], size: Int) -> [Double] {
-    var matrixT: [[Double]] = .init(repeating: .init(repeating: 0, count: size), count: size)
+func slauSolution(matrixA: [[Float]], vectorB: [Float], size: Int) -> [Float] {
+    var matrixT: [[Float]] = .init(repeating: .init(repeating: .zero, count: size), count: size)
     matrixT[0][0] = sqrt(matrixA[0][0])
     (1..<size).forEach { matrixT[0][$0] = matrixA[0][$0] / matrixT[0][0] }
     (1..<size).forEach { i in
@@ -45,12 +45,12 @@ func slauSolution(matrixA: [[Double]], vectorB: [Double], size: Int) -> [Double]
         }
     }
     
-    var y: [Double] = .init(repeating: 0, count: size)
+    var y: [Float] = .init(repeating: .zero, count: size)
     for i in 0..<size {
         y[i] = (vectorB[i] - (0..<i).map { matrixT[$0][i] * y[$0] }.sum()) / matrixT[i][i]
     }
     
-    var x: [Double] = .init(repeating: 0, count: size)
+    var x: [Float] = .init(repeating: .zero, count: size)
     for i in stride(from: size - 1, to: -1, by: -1) {
         x[i] = (y[i] - (i+1..<size).map { matrixT[i][$0] * x[$0] }.sum()) / matrixT[i][i]
     }
@@ -58,29 +58,29 @@ func slauSolution(matrixA: [[Double]], vectorB: [Double], size: Int) -> [Double]
     return x
 }
 
-let xArray: [Double] = [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
-let yArray: [Double] = [1.32, 1.81, 2.58, 2.88, 3.88, 4.29, 4.58, 5.08, 4.14]
+let xArray: [Float] = [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
+let yArray: [Float] = [1.32, 1.81, 2.58, 2.88, 3.88, 4.29, 4.58, 5.08, 4.14]
 
 let n: Int = xArray.count
 let m: Int = 4
 
 struct Polynom {
     
-    private let factors: [Double]
+    private let factors: [Float]
     
-    init(factors: [Double]) {
+    init(factors: [Float]) {
         self.factors = factors
     }
     
-    func value(by x: Double) -> Double {
+    func value(by x: Float) -> Float {
         return factors.enumerated().map { degree, factor in
             factor * x.pow(degree)
         }.sum()
     }
 }
 
-var matrixA: [[Double]] = Array(repeating: Array(repeating: .zero, count: m), count: m)
-var vectorB: [Double] = Array(repeating: .zero, count: m)
+var matrixA: [[Float]] = Array(repeating: Array(repeating: .zero, count: m), count: m)
+var vectorB: [Float] = Array(repeating: .zero, count: m)
 
 for i in 0..<m {
     
@@ -100,7 +100,7 @@ let variance = sqrt(xArray.enumerated().map { index, x in
     (function.value(by: x) - yArray[index]).pow(2)
 }.sum())
 
-let inaccuracy = variance / sqrt(Double(n))
+let inaccuracy = variance / sqrt(Float(n))
 let error = inaccuracy / sqrt(yArray.map { $0.pow(2) }.sum())
 let middleValues = xArray.pairs().map { function.value(by: ($0 + $1) / 2) }
 
