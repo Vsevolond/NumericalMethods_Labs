ef97da5c24d419d1572c891c2bbff7cbfdfcc678
Vsevolond <vsevolond@gmail.com>
2024-04-23 19:35:37 +0300

    MAIN: fixed lab5

--- NumericalMethods_Labs/lab5/main.swift
+++ NumericalMethods_Labs/lab5/main.swift
@@ -1,57 +1,54 @@
 import Foundation
 
-func f(_ point: (x1: Float, x2: Float)) -> Float {
-    let (x1, x2) = point
-    return sinf(x1.pow(2)) + x1.pow(4) + 2 * x1.pow(2) * x2.pow(4) + logf(1 + 0.1 * x1)
+func f(_ point: (Float, Float)) -> Float {
+    let (x, y) = point
+    return sinf(x.pow(2)) + x.pow(4) + 2 * x.pow(2) * y.pow(4) + logf(1 + 0.1 * x)
 }
 
-func f_x1(_ point: (x1: Float, x2: Float)) -> Float {
-    let (x1, x2) = point
-    return 2 * x1 * cosf(x1.pow(2)) + 4 * x1.pow(3) + 4 * x1 * x2.pow(4) + 0.1 / (1 + 0.1 * x1)
+func f_x(_ point: (Float, Float)) -> Float {
+    let (x, y) = point
+    return 2 * x * cosf(x.pow(2)) + 4 * x.pow(3) + 4 * x * y.pow(4) + 0.1 / (1 + 0.1 * x)
 }
 
-func f_x2(_ point: (x1: Float, x2: Float)) -> Float {
-    let (x1, x2) = point
-    return 8 * x1.pow(2) * x2.pow(3)
+func f_y(_ point: (Float, Float)) -> Float {
+    let (x, y) = point
+    return 8 * x.pow(2) * y.pow(3)
 }
 
-func f_x1_x1(_ point: (x1: Float, x2: Float)) -> Float {
-    let (x1, x2) = point
-    return -4 * x1.pow(2) * sinf(x1.pow(2)) + 2 * cosf(x1.pow(2)) + 12 * x1.pow(2) + 4 * x2.pow(4) - 0.01 / (1 + 0.1 * x1).pow(2)
+func f_xx(_ point: (Float, Float)) -> Float {
+    let (x, y) = point
+    return -4 * x.pow(2) * sinf(x.pow(2)) + 2 * cosf(x.pow(2)) + 12 * x.pow(2) + 4 * y.pow(4) - 0.01 / (1 + 0.1 * x).pow(2)
 }
 
-func f_x2_x2(_ point: (x1: Float, x2: Float)) -> Float {
-    let (x1, x2) = point
-    return 24 * x1.pow(2) * x2.pow(2)
+func f_yy(_ point: (Float, Float)) -> Float {
+    let (x, y) = point
+    return 24 * x.pow(2) * y.pow(2)
 }
 
-func f_x1_x2(_ point: (x1: Float, x2: Float)) -> Float {
-    let (x1, x2) = point
-    return 16 * x1 * x2.pow(3)
+func f_xy(_ point: (Float, Float)) -> Float {
+    let (x, y) = point
+    return 16 * x * y.pow(3)
 }
 
-func gradf(_ point: (x1: Float, x2: Float)) -> (x1: Float, x2: Float) {
-    (f_x1(point), f_x2(point))
-}
+func gradf(_ point: (Float, Float)) -> (x: Float, y: Float) { (f_x(point), f_y(point)) }
 
-var point: (x1: Float, x2: Float) = (0, 0)
-
+var point: (x: Float, y: Float) = (0, 0)
 let eps: Float = 0.001
-var grad = gradf(point)
-var k = 0
+let analytic: (x: Float, y: Float) = (-0.05, 0)
 
-while max(abs(grad.x1), abs(grad.x2)) >= eps {
-    let u1 = -f_x1(point).pow(2) - f_x2(point).pow(2)
-    let u2 = f_x1_x1(point) * f_x1(point).pow(2) + 2 * f_x1_x2(point) * f_x1(point) * f_x2(point) + f_x2_x2(point) * f_x2(point).pow(2)
+var grad = gradf(point)
+var k: Int = 0
+
+while max(abs(grad.x), abs(grad.y)) >= eps {
+    let u1 = f_x(point).pow(2) + f_y(point).pow(2)
+    let u2 = f_xx(point) * f_x(point).pow(2) + 2 * f_xy(point) * f_x(point) * f_y(point) + f_yy(point) * f_y(point).pow(2)
     
-    let t = -u1 / u2
-    
-    
-    point.x1 = point.x1 - t * f_x1(point)
-    point.x2 = point.x2 - t * f_x2(point)
+    let tMin: Float = u1 / u2
+    point = (point.x - tMin * f_x(point), point.y - tMin * f_y(point))
     grad = gradf(point)
     k += 1
 }
 
-print(point)
 print(k)
+print(point)
+print(abs(analytic.x - point.x), abs(analytic.y - point.y))
