563aab3305ffdcdb87263073f4883c6366dbf068
Vsevolond <vsevolond@gmail.com>
2024-04-24 20:41:30 +0300

    MAIN: fixed lab6

--- NumericalMethods_Labs/lab6/main.swift
+++ NumericalMethods_Labs/lab6/main.swift
@@ -5,8 +5,11 @@ func sgn(_ x: Float) -> Float {
     return x / abs(x)
 }
 
-func dividingRootEquation(of function: (Float) -> Float, in section: (a: Float, b: Float), with inaccuracy: Float) -> Float {
+func dividingRootEquation(of function: (Float) -> Float, in section: (a: Float, b: Float),
+                          with inaccuracy: Float) -> (root: Float, approximationCount: Int)
+{
     var currentSection: (a: Float, b: Float) = section
+    var approximationCount: Int = 0
     
     while currentSection.b - currentSection.a > inaccuracy {
         let middle = (currentSection.b + currentSection.a) / 2
@@ -18,14 +21,16 @@ func dividingRootEquation(of function: (Float) -> Float, in section: (a: Float, 
             currentSection.a = middle
             
         } else { fatalError("function does not have the root between section: \(section)") }
+        
+        approximationCount += 1
     }
     
     let root = (currentSection.a + currentSection.b) / 2
-    return root
+    return (root, approximationCount)
 }
 
 func tangentRootEquation(of function: (Float) -> Float, and derivative: (first: (Float) -> Float, second: (Float) -> Float),
-                         in section: (a: Float, b: Float), with inaccuracy: Float) -> Float
+                         in section: (a: Float, b: Float), with inaccuracy: Float) -> (root: Float, approximationCount: Int)
 {
     var root: (current: Float, last: Float) = (.zero, .zero)
     
@@ -39,13 +44,17 @@ func tangentRootEquation(of function: (Float) -> Float, and derivative: (first: 
         
     } else { fatalError("function does not have the root between section: \(section)") }
     
+    var approximationCount: Int = 1
+    
     while function(root.current) * function(root.current + sgn(root.current - root.last) * inaccuracy) >= 0 {
         let last = root.current
         root.current = root.last - function(root.last) / derivative.first(root.last)
         root.last = last
+        
+        approximationCount += 1
     }
     
-    return root.current
+    return (root.current, approximationCount)
 }
 
 func f(_ x: Float) -> Float { x ** 3 - 12 * x - 5 }
@@ -56,7 +65,7 @@ let sections: [(a: Float, b: Float)] = [(-4, -3), (-1, 0), (3, 4)]
 let eps: Float = 0.001
 
 sections.forEach { section in
-    let root1 = dividingRootEquation(of: f, in: section, with: eps)
-    let root2 = tangentRootEquation(of: f, and: (f_x, f_xx), in: section, with: eps)
-    print(root1, root2, abs(root1 - root2))
+    let (root1, count1) = dividingRootEquation(of: f, in: section, with: eps)
+    let (root2, count2) = tangentRootEquation(of: f, and: (f_x, f_xx), in: section, with: eps)
+    print(root1, count1, root2, count2, abs(root1 - root2))
 }
